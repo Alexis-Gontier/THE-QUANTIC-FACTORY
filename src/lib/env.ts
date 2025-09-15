e6435d5d967107516ab9f5f37d4f7bda0f02bfc4
Gontier <alexis.gontier03@gmail.com>
2025-09-15 02:02:38 +0200

    🔐 [ENV] update

---
+++ src/lib/env.ts
@@ -0,0 +1,17 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "production", "test"]),
+  },
+  client: {
+    NEXT_PUBLIC_APP_URL: z.string().url(),
+    NEXT_PUBLIC_OPEN_DATA_PARIS_API_URL: z.string().url(),
+  },
+  // For Next.js >= 13.4.4, you only need to destructure client variables:
+  experimental__runtimeEnv: {
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_OPEN_DATA_PARIS_API_URL: process.env.NEXT_PUBLIC_OPEN_DATA_PARIS_API_URL,
+  },
+});
